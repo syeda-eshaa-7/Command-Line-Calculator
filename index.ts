3e4139d1e4b4d549e285e6229460d54fa3e57177
Syeda_Esha <161529169+syeda-eshaa-7@users.noreply.github.com>
2024-03-19 13:53:25 -0700

    push my calculator to github

---
+++ index.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { messege: "Enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the opretors to preform action ",
+        type: "list",
+        name: "opretors",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+if (answer.opretors === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+} else if (answer.opretors === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+} else if (answer.opretors === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+} else if (answer.opretors === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
